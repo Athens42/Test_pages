91a16ac00d37e686a45cbdf172d42a630988594d
Athens42 <jonas_42@icloud.com>
2025-09-27 17:30:12 +0200

    Simple index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Portfolio Manager</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="app">
+        <!-- Your HTML content here -->
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
